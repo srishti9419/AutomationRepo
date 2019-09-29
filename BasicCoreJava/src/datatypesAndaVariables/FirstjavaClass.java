711126bae8b32a4ce2b52875232116412b64e066
Srishti rathore <srishtirathore@gmail.com>
2019-09-29 11:21:17 +0530

    first program

---
+++ BasicCoreJava/src/datatypesAndaVariables/FirstjavaClass.java
@@ -0,0 +1,11 @@
+package datatypesAndaVariables;
+
+public class FirstjavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to selenium");
+
+	}
+
+}
